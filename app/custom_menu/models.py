0e8c3061beff43cdd7dd6750bb6720b9836fea7a
ytec <ytec190@gmail.com>
2017-07-13 18:38:23 +0200

    models Page and Custom_menu

--- app/custom_menu/models.py
+++ app/custom_menu/models.py
@@ -2,7 +2,36 @@ from django.db import models
 from cms.models import CMSPlugin
 from cms.models import Page
 
-from app.pages.models import Pages
+from app.page.models import page, Sub_Pages
+
+class Sub_Pages_menu(models.Model):
+    name = models.CharField(max_length=50, default="Pagina")
+    Icon = models.FileField(upload_to='Custom_Menu/img',
+                            default='Custom_Menu/img/icon/default.png')
+    X = models.CharField(max_length=4, blank=True)
+    Y = models.CharField(max_length=4, blank=True)
+    page = models.ForeignKey(Sub_Pages)
+
+    def __str__(self):
+        return self.name
+
+class page_menu(models.Model):
+    name = models.CharField(max_length=50, default="Pagina")
+    Icon = models.FileField(upload_to='Custom_Menu/img',
+                            default='Custom_Menu/img/icon/default.png')
+    X = models.CharField(max_length=4, blank=True)
+    Y = models.CharField(max_length=4, blank=True)
+    page = models.ForeignKey(page)
+    Sub_Pages = models.ManyToManyField(Sub_Pages_menu)
+
+    def __str__(self):
+        return self.name
+class Pages(models.Model):
+    name = models.CharField(max_length=50, default="Pagina")
+    Pages = models.ManyToManyField(page_menu)
+
+    def __str__(self):
+        return self.name
 
 class banner(models.Model):
     name = models.CharField(max_length=50, default="Pagina")
@@ -10,6 +39,9 @@ class banner(models.Model):
     Imagen = models.FileField(upload_to='Custom_Menu/img/Banner_img',
                               default='Custom_Menu/img/Banner_img/default.png')
     url = models.URLField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=50, default="Category")
@@ -19,7 +51,6 @@ class Category(models.Model):
     url = models.URLField(max_length=100, blank=True)
     Banner = models.ManyToManyField(banner, blank=True)
     Pages = models.ManyToManyField(Pages, blank=True)
-
 
     def __str__(self):
         return self.name
--- app/pages/models.py
+++ app/pages/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from cms.models import Page
 
-from app.subpages.models import Sub_Pages
+class Sub_Pages(models.Model):
+    name = models.CharField(max_length=50, default="Pagina")
+    description = models.TextField(blank=True)
+    titel = models.CharField(max_length=100,
+                             default='Titulo pagina ')
+    url = models.URLField(max_length=100, blank=True)
+
+    page_external = models.ForeignKey(Page)
+    Level = models.CharField(max_length=3, default='1', blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class page(models.Model):
     name = models.CharField(max_length=50, default="Pagina")
@@ -9,12 +21,8 @@ class page(models.Model):
     titel = models.CharField(max_length=100,
                              default='Titulo pagina ')
     url = models.URLField(max_length=100, blank=True)
-    Icon = models.FileField(upload_to='Custom_Menu/img',
-                            default='Custom_Menu/img/icon/default.png')
-    X = models.CharField(max_length=4, blank=True)
-    Y = models.CharField(max_length=4, blank=True)
+
     page_external = models.ForeignKey(Page)
-    subpages = models.ManyToManyField(Sub_Pages, blank=True)
     Level = models.CharField(max_length=3, default='1', blank=True)
 
     def __str__(self):
